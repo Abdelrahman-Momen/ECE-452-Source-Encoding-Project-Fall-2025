2e27571a7879c6f12c7c48381b9f4e94f5bb01b6
MrMystery <113464087+crash1218@users.noreply.github.com>
2025-12-17 14:06:57 +0200

    Update IT_Project.m

--- Codes/Shannon Binary/IT_Project.m
+++ Codes/Shannon Binary/IT_Project.m
@@ -1,13 +1,12 @@
-clc; close all; clearvars; pkg load image strings datatypes statistics;
-% Read image
-##prompt = 'Enter the picture file name followed by the file extension: ';
-##image_name = input(prompt, 's');
-image = imread("pic.png");
+clc; close all; clearvars;
+
+image = imread("dog.png"); % Changed to dog.png for testing
 if ndims(image)==3                % colour image
     image = rgb2gray(image);      % 0-255 grayscale
 end
 
-[prob, I, H_2, counts] = imgstats(image);
+% CRITICAL CHANGE: Capture the new 'symbols' output
+[prob, I, H_2, counts, symbols] = imgstats(image);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -18,17 +17,12 @@ end
 [codebook_sh_bin, len_avg_sh_bin, alpha] = sh_bin_dict(prob, I);
 efficiency_sh_bin = H_2 / len_avg_sh_bin;
 fprintf("Shannon Binary Efficiency equals %.3f \n", efficiency_sh_bin);
-% % Create formatted table
-##alpha_bin = "0." + codebook_sh_bin;
-##Bits = codebook_sh_bin;
-##pixel_vals = (0:length(codebook_sh_bin)-1)';
-##T = table(pixel_vals, alpha, alpha_bin, Bits, 'VariableNames', {'values', 'alpha', 'alpha_bin', 'Bits'});
-##disp(T);
 
 % Convert image color values to bits
 [r, c] = size(image);
-encoded_seq = sh_bin_encoder(codebook_sh_bin, image);
 
+% CRITICAL CHANGE: Pass the 'symbols' input to the encoder
+[encoded_seq, loc] = sh_bin_encoder(codebook_sh_bin, image, symbols); 
 
 
 % ###########################################################################
@@ -37,24 +31,12 @@ encoded_seq = sh_bin_encoder(codebook_sh_bin, image);
 % ###########################################################################
 % ###########################################################################
 
-decoded_sequence = sh_bin_decoder(codebook_sh_bin, r, c, encoded_seq);
+decoded_sequence = sh_bin_decoder(codebook_sh_bin, r, c, encoded_seq, symbols);
 
-if image(:) == decoded_sequence(:)
+if all(image(:) == decoded_sequence(:)) % Using all() for array comparison
     disp('Reconstruction OK')
     figure;
     imshow(decoded_sequence)
 else
     disp('False')
 end
-
-
-
-
-
-
-
-
-
-
-
-
